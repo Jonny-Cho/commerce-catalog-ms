7b73fd5615cbc9fc9062779353794ef52412ce3e
Jonny-Cho <andante2183@gmail.com>
2022-01-15 15:19:48 +0900

    setting: 프로젝트 세팅, Entity, Repository, sql 작성

---
+++ src/main/java/com/commerce/entity/CatalogRepository.java
@@ -0,0 +1,7 @@
+package com.commerce.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+    CatalogEntity findByProductId(String productId);
+}
